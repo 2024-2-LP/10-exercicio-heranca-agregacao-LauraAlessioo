aa0166309ce43540c4ccfc1fbe97581973626af6
LauraAlessioo <laura.carvalho@sptech.school>
2024-10-31 13:17:23 -0300

    No processo de finalização

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,67 @@
 package school.sptech;
 
-public class Consultoria {
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consultoria extends DesenvolvedorWeb {
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores;
+
+    public Consultoria(String nome, Integer vagas) {
+        this.nome = nome;
+        this.vagas = vagas;
+        this.desenvolvedores = new ArrayList<>();
+    }
+
+    public Consultoria() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+    public Boolean contratar(Desenvolvedor desenvolvedor){
+        if (vagas > desenvolvedores.size()){
+            desenvolvedores.add(desenvolvedor);
+            return true;
+        }return false;
+    }
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor){
+        if (desenvolvedor.isFullstack()){
+          contratar(desenvolvedor);
+          return true;
+        }
+        return false;
+    }
+    public Double getTotalSalarios(){
+        Double total = 0.0;
+        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores){
+            total += desenvolvedorDaVez.calcularSalario();
+        }return total;
+    }
+    public Integer qtdDesenvolvedoresMobile(){
+        Integer cont = 0;
+        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores){
+            if (desenvolvedorDaVez instanceof DesenvolvedorMobile){
+                cont++;
+        }
+        }return cont;
+
+    }
+
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
@@ -1,4 +1,39 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorMobile {
+import school.sptech.Desenvolvedor;
+
+import java.lang.invoke.StringConcatFactory;
+
+public class DesenvolvedorMobile extends Desenvolvedor {
+    private String plataforma;
+    private String linguagem;
+    private Integer horasPrototipacao;
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public void setLinguagem(String linguagem) {
+        this.linguagem = linguagem;
+    }
+
+    public Integer getHorasPrototipacao() {
+        return horasPrototipacao;
+    }
+
+    public void setHorasPrototipacao(Integer horasPrototipacao) {
+        this.horasPrototipacao = horasPrototipacao;
+    }
+    public Double calcularSalario(){
+        Integer valorHoraProt = 200;
+        return (qtdHoras * valorHora) + horasPrototipacao * valorHoraProt;
+    }
 }
