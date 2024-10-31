f6785e27932f1d349eed472411b4fc17ac31a663
LauraAlessioo <laura.carvalho@sptech.school>
2024-10-31 14:34:39 -0300

    Atividade finalizada

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -35,33 +35,92 @@ public class Consultoria extends DesenvolvedorWeb {
     public void setVagas(Integer vagas) {
         this.vagas = vagas;
     }
-    public Boolean contratar(Desenvolvedor desenvolvedor){
-        if (vagas > desenvolvedores.size()){
+
+    public Boolean contratar(Desenvolvedor desenvolvedor) {
+        if (vagas > desenvolvedores.size()) {
             desenvolvedores.add(desenvolvedor);
             return true;
-        }return false;
-    }
-    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor){
-        if (desenvolvedor.isFullstack()){
-          contratar(desenvolvedor);
-          return true;
         }
         return false;
     }
-    public Double getTotalSalarios(){
-        Double total = 0.0;
-        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores){
-            total += desenvolvedorDaVez.calcularSalario();
-        }return total;
-    }
-    public Integer qtdDesenvolvedoresMobile(){
-        Integer cont = 0;
-        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores){
-            if (desenvolvedorDaVez instanceof DesenvolvedorMobile){
-                cont++;
-        }
-        }return cont;
 
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor) {
+        if (desenvolvedor.isFullstack()) {
+            contratar(desenvolvedor);
+            return true;
+        }
+        return false;
+    }
+
+    public Double getTotalSalarios() {
+        Double total = 0.0;
+        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores) {
+            total += desenvolvedorDaVez.calcularSalario();
+        }
+        return total;
+    }
+
+    public Integer qtdDesenvolvedoresMobile() {
+        Integer cont = 0;
+        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores) {
+            if (desenvolvedorDaVez instanceof DesenvolvedorMobile) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+    public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario) {
+        List<Desenvolvedor> maiorSalario = new ArrayList<>();
+        for (Desenvolvedor desenvolvedorSalMenor : desenvolvedores) {
+            if (desenvolvedorSalMenor.calcularSalario() >= salario) {
+                maiorSalario.add(desenvolvedorSalMenor);
+            }
+        }
+        return maiorSalario;
+    }
+
+    public Desenvolvedor buscarMenorSalario() {
+        if (desenvolvedores.isEmpty()) {
+            return null;
+        }
+        Desenvolvedor menorSalarioDev = desenvolvedores.get(0);
+
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev.calcularSalario().compareTo(menorSalarioDev.calcularSalario()) < 0) {
+                menorSalarioDev = dev;
+            }
+        }
+        return menorSalarioDev;
+    }
+    public List<Desenvolvedor> buscarPorTecnologia(String tecnologia) {
+        List<Desenvolvedor> devsComTecnologia = new ArrayList<>();
+
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev instanceof DesenvolvedorWeb) {
+                DesenvolvedorWeb devWeb = (DesenvolvedorWeb) dev;
+                if (tecnologia.equalsIgnoreCase(devWeb.getFrontend()) || tecnologia.equalsIgnoreCase(devWeb.getBackend()) || tecnologia.equalsIgnoreCase(devWeb.getSgbd())) {
+                    devsComTecnologia.add(devWeb);
+                }
+            } else if (dev instanceof DesenvolvedorMobile) {
+                DesenvolvedorMobile devMobile = (DesenvolvedorMobile) dev;
+                if (tecnologia.equalsIgnoreCase(devMobile.getPlataforma()) || tecnologia.equalsIgnoreCase(devMobile.getLinguagem())) {
+                    devsComTecnologia.add(devMobile);
+                }
+            }
+        }
+        return devsComTecnologia;
+    }
+    public Double getTotalSalariosPorTecnologia(String tecnologia) {
+        Double totalSalarios = 0.0;
+
+        List<Desenvolvedor> devsComTecnologia = buscarPorTecnologia(tecnologia);
+
+        for (Desenvolvedor dev : devsComTecnologia) {
+            totalSalarios += dev.calcularSalario();
+        }
+
+        return totalSalarios;
     }
 
 }
